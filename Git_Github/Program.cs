e8ffbb91fb5ff5d5aed5fa6744f5fb2810346807
Edson Loiola <edson.monteiro.l@gmail.com>
2020-06-16 23:03:12 -0300

    Novas alteracoes para subir projeto no GitHub

--- Git_Github/Program.cs
+++ Git_Github/Program.cs
@@ -10,22 +10,9 @@ namespace Git_Github
             Console.WriteLine("Bom dia!");
             Console.WriteLine("Edson Monteiro Loiola");
 
-            //na pasta onde tem a solução do projeto
-            //criar um txt chamado git.txt
-            // abrir cmd nesta pasta e renomear o arquivo:  ren git.txt .gitignore
-            //abrir com bloco de notas
-            //escrever quais arquivos vão ser ignorados de um projeto de VS (pesquisar no google: gitignore visual studio)
-            //colcar nesse bloco e salvar
-            // abrir git bash nesta pasta do projeto
-            // ex: cd C:/Users/eloiola/source/Curso
-            // dar um git init  --vai criar uma pasta oculta chamada git dentro do projeto que é o repositorio git
+          
 
-
-            //identificar o usuraiono github
-            //git config --global user.name "Seu Nome" 
-            //git config --global user.email "Seu Email"
-
-            //CONSULTAR PDF DA SECAO 8 NO MATERIAL DE APIO
+            //CONSULTAR PDF DA SECAO 8 NO MATERIAL DE APIO SOBRE GIT E GITHUB
 
 
 
